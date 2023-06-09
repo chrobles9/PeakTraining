da9f9445149b08554e348947ae942ec0e487f078
Carlos Robles <chrobles@pm.me>
2023-06-09 14:49:38 -0400

    MapBox ID updated.

--- scripts/mapScript.js
+++ scripts/mapScript.js
@@ -4,8 +4,8 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyb2JsZXMiLCJhIjoiY2xpb3Z5eWZuMGk0dDNxcGd2dWdvMm5zbSJ9.UPYb5dcrvBEmpfwAu0S9eA';
 
 // New map 
-var map = new mapboxgl.Map({
-  container: 'map', // container ID
+let map = new mapboxgl.Map({
+  container: 'peakMap', // container ID
   style: 'mapbox://styles/mapbox/streets-v12', // style URL
   center: [-78.647762, 35.734885], // starting position [lng, lat]
   zoom: 13 // starting zoom
