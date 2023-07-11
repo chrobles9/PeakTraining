c5ee013465a046f804194fb6c709b1714041e24a
Carlos Robles <chrobles@pm.me>
2023-07-11 17:35:11 -0400

    Mobile menu styles and functionality updated.

--- scripts/navScript.js
+++ scripts/navScript.js
@@ -1,1 +1,10 @@
-// TODO: Add script code for collasping navigation 
+// Mobile menu script
+function mobileMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navList") {
+        x.className += " mobileMenu";
+    } else {
+        x.className = "navList";
+    }
+}
+
